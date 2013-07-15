0bd63f677b225a7c0a1cbcf7e9256f112d6564a9
Yuri Korshev <ykorshev@softgears.ru>
2013-07-15 13:46:27 +1000

    Запилен почти вся верстка сайта. Нужно сделать страницу для оптовиков и все скрипты.

---
+++ Octopus.Doors/Controllers/MainController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Octopus.Doors.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Main/
+
+        /// <summary>
+        /// Главная страница
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Отображает фрагмент главной страницы
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult IndexPart()
+        {
+            return PartialView();
+        }
+
+        /// <summary>
+        /// Отображает фрагмент стандартной двери
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult StandartPart()
+        {
+            return PartialView();
+        }
+
+        /// <summary>
+        /// Отображает фрагмент премиум страницы
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult PremiumPart()
+        {
+            return PartialView();
+        }
+
+        /// <summary>
+        /// Отображает фрагмент страницы с противопожарными дверями
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult FireProofPart()
+        {
+            return PartialView();
+        }
+
+        /// <summary>
+        /// Отображает фрагмент страницы  со статьями
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ArticlesPart()
+        {
+            return PartialView();
+        }
+
+        public ActionResult AboutPart()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Octopus.Doors/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Octopus.Doors.MvcApplication" Language="C#" %>
